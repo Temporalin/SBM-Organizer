b5e9fda9b181e4cb2dce1950763965fc76961b22
Temporalin <temporalintendo@gmail.com>
2016-01-19 11:41:56 +0100

    Renovado Internal

--- SBM-Organizer/src/SBMO/Internal.java
+++ SBM-Organizer/src/SBMO/Internal.java
@@ -2,104 +2,190 @@ package SBMO;
 
 import java.util.*;
 import challonge.model.Match;
+import challonge.model.Participant;
+import challonge.model.Tournament;
+import challonge.model.TournamentType;
+import challonge.request.*;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /* Aquí iría la gestión interna del programa */
 /* Metemos enfrentamientos en las setups */
-
 public class Internal {
-    public static String apiKey;
-    public static String url;
-    public static int nSetups;
-    public static Queue<Match> colaEnfrentamientos; // Cola con TODOS los enfrentamientos
-    
-    public static Match[] enfrentamientosSetups; // Lista de matches con los enfrentamientos para cada setup
-    public static List<Match> listaFinalizados = new ArrayList(); // Lista de matches finalizados
+
+    private String apiKey;
+    private int nSetups;
+    private Challonge challonge;
+    private Tournament tournament;
+
+    private Map<Integer, Participant> mapaJugadores; //Mapa de jugadores. Clave: id tipo int. Valor: jugdador tipo Participant.
+    private Match[] enfrentamientosSetups; // Lista de matches con los enfrentamientos para cada setup
+    private List<Match> listaFinalizados = new ArrayList(); // Lista de matches finalizados
+    private Queue<Match> colaEnfrentamientos; // Cola con TODOS los enfrentamientos
+
+    public Internal(String apiKey, int nSetups) {
+        this.apiKey = apiKey;
+        this.nSetups = nSetups;
+        this.challonge = new Challonge(apiKey);
+        mapaJugadores = new HashMap<>();
+    }
+
+    public Tournament createStandardTournament(String name, String url) {
+        CreateTournamentRequest.Builder ctr = new CreateTournamentRequest.Builder(name,TournamentType.DOUBLE_ELIMINATION,url);
+        tournament = challonge.createTournament(ctr.build());
+
+        return tournament;
+    }
+
+    public Participant addParticipant(String name, int seed) {
+        CreateParticipantRequest.Builder cprb = new CreateParticipantRequest.Builder(tournament.getUrl(), name);
+        cprb.withSeed(seed);
+
+        Participant p = challonge.createParticipant(cprb.build());
+        mapaJugadores.put(p.getId(), p);
+
+        return p;
+    }
+
+    public void startTournament() {
+        StartTournamentRequest str = new StartTournamentRequest(tournament.getUrl(),false,false);
         
-    public Internal(){}
+        challonge.startTournament(str);
+        
+        colaEnfrentamientos = new LinkedList<>();
+        
+        ListMatchRequest.Builder lmrb = new ListMatchRequest.Builder(tournament.getUrl());
+        lmrb.withState("open");
+        List<Match> listaPartidas = challonge.listMatches(lmrb.build());
+        
+        for(Match m:listaPartidas){
+            colaEnfrentamientos.add(m);
+        }
+        
+    }
+
+    public void deleteTournament() {
+        DeleteTournamentRequest dtr = new DeleteTournamentRequest(tournament.getUrl());
+        
+        challonge.deleteTournament(dtr);
+    }
     
-    /**************/
+    
+    public Match peekNextMatch(){
+        return colaEnfrentamientos.peek();
+    }
+    
+    public Match pollNextMatch(){
+        return colaEnfrentamientos.poll();
+    }
+    
+    public boolean hasNextMatch(){
+        return colaEnfrentamientos.size()>0;
+    }
+    
+    /**
+     * ***********
+     */
     /* GET Y SETS */
-    /**************/
-    
-    public static String getApiKey() {
+    /**
+     * ***********
+     */
+    public String getApiKey() {
         return apiKey;
     }
 
-    public static void setApiKey(String a) {
+    public void setApiKey(String a) {
         apiKey = a;
     }
 
-    public static String getUrl() {
-        return url;
-    }
-
-    public static void setUrl(String u) {
-        url = u;
-    }
-
-    public static int getnSetups() {
+    public int getnSetups() {
         return nSetups;
     }
 
-    public static void setnSetups(int n) {
+    public void setnSetups(int n) {
         nSetups = n;
     }
 
-    public static void setColaEnfrentamientos(Queue<Match> c) {
+    public void setColaEnfrentamientos(Queue<Match> c) {
         colaEnfrentamientos = c;
     }
-    
-    public static Queue<Match> getColaEnfrentamientos() {
+
+    public Queue<Match> getColaEnfrentamientos() {
         return colaEnfrentamientos;
     }
 
-    public static void setEnfrentamientosSetups(Match[] e) {
-        enfrentamientosSetups = e;
+    public void setEnfrentamientosSetups(Match[] e) {
+        setEnfrentamientosSetups(e);
     }
 
-    public static Match[] getEnfrentamientosSetups() {
+    public Match[] getEnfrentamientosSetups() {
         return enfrentamientosSetups;
     }
 
-    public static List<Match> getListaFinalizados() {
+    public List<Match> getListaFinalizados() {
         return listaFinalizados;
     }
 
-    public static void setListaFinalizados(List<Match> listaFinalizados) {
-        Internal.listaFinalizados = listaFinalizados;
+    public void setListaFinalizados(List<Match> listaFinalizados) {
+        this.listaFinalizados = listaFinalizados;
     }
 
-    
-    /**************/
+    /**
+     * ***********
+     */
     /* END GETSET */
-    /**************/
-    
-    // Inicializamos cola de enfrentamientos
-    public static void setUp(List<Match> enfrentamientos){
-        setColaEnfrentamientos(new LinkedList(enfrentamientos));       
-    }
-    
+    /**
+     * ***********
+     */
     // Inicialmente rellenamos las setups (que están vacías)
-    public static void initializeSetups(){
-    
+    public void initializeSetups() {
+
         Match[] listaS = new Match[getnSetups()];
-        
-        for(int i=0;i<getnSetups();i++)
+
+        for (int i = 0; i < getnSetups(); i++) {
             listaS[i] = getColaEnfrentamientos().poll();
-        
+        }
+
         setEnfrentamientosSetups(listaS);
     }
-    
+
     // Rellenamos las setups vacías
-    public static void updateSetups(){
+    public void updateSetups() {
 
         //Sacamos elementos de la cola y los metemos en el array
-        for(int i=0;i<getnSetups();i++)
-            if(getEnfrentamientosSetups()[i] == null) // Si está vacío...
+        for (int i = 0; i < getnSetups(); i++) {
+            if (getEnfrentamientosSetups()[i] == null) // Si está vacío...
+            {
                 getEnfrentamientosSetups()[i] = getColaEnfrentamientos().poll();
-        
+            }
+        }
+
     }
-    //public Match[] actualizarSetups(){}
     
-    
+    public static void main(String[] args){
+        
+        Internal internal = new Internal("7qFUMnSyDCqoES42Kvh0mJJnYOwgma1wtGZRbdYn",4);
+        Tournament t = internal.createStandardTournament("testTournament","test"+Double.toString(Math.random()*100).replace('.', '_'));
+        for(int i=1;i<=32;i++){
+            internal.addParticipant("J"+i,i);
+        }
+        internal.startTournament();
+        
+        while(internal.hasNextMatch()){
+            Match m = internal.pollNextMatch();
+            Participant one = internal.mapaJugadores.get(m.getPlayerOneId());
+            Participant two = internal.mapaJugadores.get(m.getPlayerTwoId());
+            
+            System.out.println(one.getName()+"  vs  "+two.getName());
+        }
+        
+        System.out.println("Finalizado torneo, comprueba todo en Challonge y pulsa enter");
+        try {
+            System.in.read();
+            internal.deleteTournament();
+        } catch (IOException ex) {
+            Logger.getLogger(Internal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
