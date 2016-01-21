1f8edd1c63b139f8e77a576e242f22797db2a974
Marah <carlos.maria_rodriguez@hotmail.com>
2016-01-22 00:36:37 +0100

    Quitadas cosas inutiles de Setup

--- SBM-Organizer/src/SBMO/Setup.java
+++ SBM-Organizer/src/SBMO/Setup.java
@@ -6,46 +6,20 @@ import challonge.model.Participant;
 public class Setup {
     
     private int number;
-    private String name;
     private Participant one;
     private Participant two;
     private Match match;
-
-    public Setup(int number, String name, Participant one, Participant two, Match match) {
-        this.number = number;
-        this.name = name;
-        this.one = one;
-        this.two = two;
-        this.match = match;
-    }
     
     public Setup(int number, Participant one, Participant two, Match match) {
         this.number = number;
-        this.name = "Setup "+number;
         this.one = one;
         this.two = two;
         this.match = match;
     }
 
-    
     /* GET SET */
-    
-    // *** SETS SOBRAN?
-    
     public int getNumber() {
         return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public Participant getOne() {
@@ -70,7 +44,5 @@ public class Setup {
 
     public void setMatch(Match match) {
         this.match = match;
-    }
-    
-    
+    }  
 }
